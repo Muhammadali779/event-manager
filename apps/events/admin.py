914199781087e1ed5e7b830a87a53ffb1ff51e4e
Muhammadali <baxtiyorovich779@gmail.com>
2026-02-11 21:37:33 +0500

    events done

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Event
+
+
+@admin.register(Event)
+class UserAdminModel(admin.ModelAdmin):
+    list_display = ['title', 'description', 'start_date', 'end_date', 'location', 'image', 'is_active']
+    
